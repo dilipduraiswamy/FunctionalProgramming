d1ba49b3925c3dd073dbb9c838aa39c8c0430673
Dilip <dilipduraiswamy@gmail.com>
2021-12-23 22:23:01 +0530

    Function chaining and Function

--- src/com/dilip/FunctionExample.java
+++ src/com/dilip/FunctionExample.java
@@ -10,10 +10,20 @@ public class FunctionExample {
         int increment2=incrementByOneFunction.apply(0);
         System.out.println(increment1);
         System.out.println(increment2);
+        int multiply1=multiplyBy10Function.apply(increment2);
+        System.out.println(multiply1);
+
+        //combine both addition and multiply i.e function chains
+        int addAndMultiply=incrementByOneFunction
+                .andThen(multiplyBy10Function)
+                .apply(0);
+        System.out.println(addAndMultiply);
     }
 
     //Function takes two parameter i.e input and output types
     static Function<Integer,Integer> incrementByOneFunction=number->number+1;
+
+    static Function<Integer,Integer> multiplyBy10Function=number->number*10;
 
     static int incrementByOne(int number){
         return number+1;
