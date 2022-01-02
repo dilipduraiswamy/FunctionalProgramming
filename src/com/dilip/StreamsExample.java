3d466382c3d4ecd6a35d140aa03b4266d0234482
Dilip <dilipduraiswamy@gmail.com>
2022-01-02 21:44:14 +0530

    Streams Additional Methods Example

--- src/com/dilip/StreamsExample.java
+++ src/com/dilip/StreamsExample.java
@@ -4,6 +4,7 @@ import com.dilip.dto.GenderEnum;
 import com.dilip.dto.Person;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,11 +15,11 @@ public class StreamsExample {
     public static void main(String[] args) {
 
         List<Person> people= List.of(
-                new Person("dilip",MALE),
-                new Person("asha",FEMALE),
-                new Person("duraiswamy",MALE),
-                new Person("rani",FEMALE),
-                new Person("brownie",MALE)
+                new Person("dilip",MALE,27),
+                new Person("asha",FEMALE,45),
+                new Person("duraiswamy",MALE,56),
+                new Person("rani",FEMALE,52),
+                new Person("brownie",MALE,10)
         );
 
        people.stream()
@@ -28,6 +29,35 @@ public class StreamsExample {
 
        boolean isFemalesAvailable=people.stream().anyMatch(person -> FEMALE.equals(person.gender));
        System.out.println(isFemalesAvailable);
+
+
+       List<Person> sortedPeoplesDescByAge= people.stream()
+               .sorted(Comparator.comparing(Person::getAge))
+               .collect(Collectors.toList());
+
+       List<Person> sortedPeoplesAscByAge= people.stream()
+                .sorted(Comparator.comparing(Person::getAge).reversed())
+                .collect(Collectors.toList());
+
+
+        System.out.println("Descending Order Print By Age");
+        sortedPeoplesDescByAge.stream()
+               .forEach(System.out::println);
+
+        System.out.println("Ascending Order Print By Age");
+        sortedPeoplesAscByAge.stream()
+                .forEach(System.out::println);
+
+        System.out.println("Print max aged person");
+        people.stream()
+                .max(Comparator.comparing(Person::getAge))
+                .ifPresent(System.out::println);
+
+        System.out.println("Print min aged person");
+        people.stream()
+                .min(Comparator.comparing(Person::getAge))
+                .ifPresent(System.out::println);
+
     }
 
 
--- src/com/dilip/dto/Person.java
+++ src/com/dilip/dto/Person.java
@@ -4,10 +4,17 @@ public class Person {
 
     private final String name;
     public final GenderEnum gender;
+    private Integer age;
 
     public Person(String name, GenderEnum gender) {
         this.name = name;
         this.gender = gender;
+    }
+
+    public Person(String name, GenderEnum gender,Integer age) {
+        this.name = name;
+        this.gender = gender;
+        this.age=age;
     }
 
     @Override
@@ -16,5 +23,22 @@ public class Person {
                 "name='" + name + '\'' +
                 ", gender=" + gender +
                 '}';
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public GenderEnum getGender() {
+        return gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
     }
 }
